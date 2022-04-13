d3f1e6a538a982e9a8de28a542a44153bad2473d
André Santiago <82945862+Dresovense@users.noreply.github.com>
2022-04-13 11:21:04 +0200

    afficher 9 images aléatoires

--- smallgames/imports/ui/components/jeuImages/afficherImage/afficherImage.js
+++ smallgames/imports/ui/components/jeuImages/afficherImage/afficherImage.js
@@ -3,8 +3,22 @@ import { SourceImage } from '../../../../api/sourceImages.js';
 
 import './afficherImage.html';
 
+
 Template.afficherImage.helpers({
     sourceImage(){
-        return SourceImage.findOne({}).source;
+        let listeImagesAide = [];
+        let nombresRandom = [];
+        for(let i = 0; i < 9; i++)
+        {
+            let random = Math.floor(Math.random()*9);
+            if(nombresRandom.includes(random)){
+                i--;
+            }
+            else{
+                nombresRandom.push(random);
+                listeImagesAide.push(SourceImage.findOne({type: "aide"}, {skip: random}).source);
+            }
+        }
+        return listeImagesAide;
     },
 });
--- smallgames/server/main.js
+++ smallgames/server/main.js
@@ -4,8 +4,18 @@ import { SourceImage } from '../imports/api/sourceImages.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
-  if(SourceImage.find().count() === 0)
-  {
-    SourceImage.insert({ source:'/jeuImages/reponses/moto.jpg', nom:'moto', type:'reponse'});
+  if (SourceImage.find().count() === 0) {
+        SourceImage.insert({source:'/jeuImages/aides/argent.jpg', nom:'argent', type:'aide'});
+        SourceImage.insert({source:'/jeuImages/aides/armee.jpg', nom:'armee' , type:'aide'});
+        SourceImage.insert({source:'/jeuImages/aides/bois.jpg', nom:'bois' , type:'aide' });
+        SourceImage.insert({source:'/jeuImages/aides/cafe.jpg', nom:'cafe' , type:'aide'});
+        SourceImage.insert({source:'/jeuImages/aides/carte.jpg', nom:'carte' , type:'aide' });
+        SourceImage.insert({source:'/jeuImages/aides/eau.jpg', nom:'eau' , type:'aide' });
+        SourceImage.insert({source:'/jeuImages/aides/eclair.jpg', nom:'eclair' , type:'aide'});
+        SourceImage.insert({source:'/jeuImages/aides/pomme.jpg', nom:'pomme' , type:'aide' });
+        SourceImage.insert({source:'/jeuImages/aides/tempeteMer.jpg', nom:'tempeteMer' , type:'aide'});
+        SourceImage.insert({source:'/jeuImages/aides/unil.jpg', nom:'unil' , type:'aide' });
+        SourceImage.insert({source:'/jeuImages/reponses/moto.jpg', nom:'moto' , type:'reponse' });
+        SourceImage.insert({source:'/jeuImages/reponses/hockey.jpg', nom:'hockey' , type:'reponse'});
   }
 });
