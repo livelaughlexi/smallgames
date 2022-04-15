dca16e5169db3b6dd1de52aa3d04f24c08e2721f
André Santiago <82945862+Dresovense@users.noreply.github.com>
2022-04-15 20:06:45 +0200

    sélectionner Image

--- smallgames/imports/ui/components/jeuImages/afficherImage/afficherImage.js
+++ smallgames/imports/ui/components/jeuImages/afficherImage/afficherImage.js
@@ -10,15 +10,28 @@ Template.afficherImage.helpers({
         let nombresRandom = [];
         for(let i = 0; i < 9; i++)
         {
-            let random = Math.floor(Math.random()*9);
+            let random = Math.floor(Math.random()*10);   //prendre des images aléatoires
             if(nombresRandom.includes(random)){
                 i--;
             }
             else{
                 nombresRandom.push(random);
-                listeImagesAide.push(SourceImage.findOne({type: "aide"}, {skip: random}).source);
+                listeImagesAide.push([SourceImage.findOne({type: "aide"}, {skip: random}).source]); //prend uniquement les images de type aide
             }
         }
         return listeImagesAide;
     },
 });
+
+Template.afficherImage.events({
+    "click .aideImage"(event) {
+       const target = event.target;
+        if(target.style.filter == "opacity(100%)")
+        {
+            target.style.filter = "opacity(50%)";   //séléctionner
+        }
+        else{
+            target.style.filter = "opacity(100%)";  //désélectionner
+        }
+    }
+})
