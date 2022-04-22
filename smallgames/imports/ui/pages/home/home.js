d3e33bba12bb5e49129951de176b7e5d35b01d31
Aarzawa <marie.fasel@unil.ch>
2022-04-22 16:57:36 +0200

    basic login

--- smallgames/imports/ui/pages/home/home.js
+++ smallgames/imports/ui/pages/home/home.js
@@ -1,4 +1,12 @@
-import { Template } from 'meteor/templating';
+//import { Template } from 'meteor/templating';
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
 
 import './home.html';
+if (Meteor.isClient) {
 
+    Accounts.ui.config({
+        passwordSignupFields: 'USERNAME_AND_EMAIL'
+    });
+
+}
